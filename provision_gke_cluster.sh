db967a93eaca7b3a1bec890baa6eb75bf030facc
ambarish2012 <ambarish@shippable.com>
2017-10-23 15:09:22 -0700

    update sample

--- provision_gke_cluster.sh
+++ provision_gke_cluster.sh
@@ -1,4 +1,13 @@
 # This script provisions a GKE cluster
 #! /bin/bash -e
 
-gcloud container clusters create $1 --num-nodes=1 --machine-type=n1-standard-1
+# check if the cluster already exists
+response=$(gcloud container clusters describe $1 --zone=$2 || echo "ClusterNotFound")
+
+if [[ $response = "ClusterNotFound" ]]
+then
+  echo "Creating cluster "$1
+  gcloud container clusters create $1 --num-nodes=1 --machine-type=n1-standard-1
+else
+  echo "Cluster "$1" already exists"
+fi
