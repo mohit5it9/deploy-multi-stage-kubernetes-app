d9dccc00ef9101d9158c9e1f28fb84def00781a2
mohit5it9 <mohit@shippable.com>
2017-10-27 15:16:23 +0530

    updates to use gcloudKey integratopn

--- provision_gke_cluster.sh
+++ provision_gke_cluster.sh
@@ -7,7 +7,7 @@ response=$(gcloud container clusters describe $1 --zone=$2 || echo "ClusterNotFo
 if [[ $response = "ClusterNotFound" ]]
 then
   echo "Creating cluster "$1
-  gcloud container clusters create $1 --num-nodes=1 --machine-type=n1-standard-1
+  gcloud container clusters create $1 --num-nodes=1 --machine-type=n1-standard-1 --zone=$2
 else
   echo "Cluster "$1" already exists"
 fi
